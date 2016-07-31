2db594c82abc9f209db83bd3b5db9eddfe8572e9
Adi <adrian.rus@live.com>
2016-07-31 14:50:42 +0100

    database setting amend

--- settings/staging.py
+++ settings/staging.py
@@ -1,12 +1,13 @@
 from base import *
 import dj_database_url
+import settings
 
 DEBUG = True
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'mydatabase',
     }
 }
 
